bba0b7f187f95d98b4a26ade6a20e850da55f412
NinaKWelch <ninakwelch@gmail.com>
2025-02-10 17:13:02 +0000

    exercise 5.19

--- tests/bloglist_app.spec.js
+++ tests/bloglist_app.spec.js
@@ -1,9 +1,10 @@
 const { test, expect, beforeEach, describe } = require('@playwright/test')
+const { loginWith, createBlog } = require('./helper')
 
 describe('Blog app', () => {
   beforeEach(async ({ page, request }) => {
-    await request.post('http://localhost:3003/api/testing/reset')
-    await request.post('http://localhost:3003/api/users', {
+    await request.post('/api/testing/reset')
+    await request.post('/api/users', {
       data: {
         name: 'John Doe',
         username: 'john',
@@ -11,7 +12,7 @@ describe('Blog app', () => {
       }
     })
 
-    await page.goto('http://localhost:5173')
+    await page.goto('/')
   })
 
   test('Front page can be opened', async ({ page }) => {
@@ -33,28 +34,15 @@ describe('Blog app', () => {
 
   describe('Login', () => {
     test('succeeds with correct credentials', async ({ page }) => {
-      const button = page.getByRole('button', { name: 'login' })
-      const usernameField = page.getByTestId('username')
-      const passwordField = page.getByTestId('password')
-  
-      await button.click()
-      await usernameField.fill('john')
-      await passwordField.fill('johnpw')
-      await button.click()
+      await loginWith(page, 'john', 'johnpw')
 
       await expect(page.getByText('John Doe logged in')).toBeVisible()
     })
 
     test('fails with wrong credentials', async ({ page }) => {
-      const button = page.getByRole('button', { name: 'login' })
-      const usernameField = page.getByTestId('username')
-      const passwordField = page.getByTestId('password')
       const errorMessage = page.locator('.error')
-  
-      await button.click()
-      await usernameField.fill('john')
-      await passwordField.fill('pw')
-      await button.click()
+
+      await loginWith(page, 'john', 'pw')
 
       await expect(page.getByText('John Doe logged in')).not.toBeVisible()
      
@@ -62,6 +50,18 @@ describe('Blog app', () => {
       await expect(errorMessage).toContainText('Wrong credentials')
       await expect(errorMessage).toHaveCSS('border-style', 'solid')
       await expect(errorMessage).toHaveCSS('color', 'rgb(255, 0, 0)')
+    })
+  })
+
+  describe('When logged in', () => {
+    beforeEach(async ({ page }) => {
+      await loginWith(page, 'john', 'johnpw')
+    })
+  
+    test('a new blog can be created', async ({ page }) => {
+      await createBlog(page, 'test blog title', 'test blog author', 'test blog url')
+
+      await expect(page.getByText('test blog title, by test blog author')).toBeVisible()
     })
   })
 })
---
+++ tests/helper.js
@@ -0,0 +1,26 @@
+const loginWith = async (page, username, password)  => {
+  const button = page.getByRole('button', { name: 'login' })
+  const usernameField = page.getByTestId('username')
+  const passwordField = page.getByTestId('password')
+
+  await button.click()
+  await usernameField.fill(username)
+  await passwordField.fill(password)
+  await button.click()
+}
+
+const createBlog = async (page, title, author, url) => {
+  const button = page.getByRole('button', { name: 'create new' })
+  const titleField = page.getByTestId('title')
+  const authorField = page.getByTestId('author')
+  const urlField = page.getByTestId('url')
+  const submitButton = page.getByRole('button', { name: 'create' })
+
+  await button.click()
+  await titleField.fill(title)
+  await authorField.fill(author)
+  await urlField.fill(url)
+  await submitButton.click()
+}
+
+export { loginWith, createBlog }
